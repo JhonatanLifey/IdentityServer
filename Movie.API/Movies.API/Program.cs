a4ae258621004f4a4020d5fc2f8a0cdb4fa0a62f
Jhonatan Carranza Conde <jhonatanlifey@outlook.com>
2022-09-27 16:58:31 +0930

    Movi API - CRUD Finish

--- Movie.API/Movies.API/Program.cs
+++ Movie.API/Movies.API/Program.cs
@@ -1,4 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<MoviesAPIContext>(options =>
+    options.UseInMemoryDatabase("Movies"));
 
 // Add services to the container.
 
@@ -8,6 +16,21 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+SeedDatabase(app);
+
+
+static void SeedDatabase(IHost app)
+{
+	using var scope = app.Services.CreateScope();
+	var services = scope.ServiceProvider;
+	var movieContext = services.GetRequiredService<MoviesAPIContext>();
+	MoviesAPIContextSeed.SeedAsync(movieContext);
+}
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
